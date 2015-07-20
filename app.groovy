086d773ec3943398072fe5321158d48e15d747b0
Dan Woods <daniel.p.woods@gmail.com>
2015-07-19 23:40:06 -0400

    COMMIT #4 - The when Handler

--- app.groovy
+++ app.groovy
@@ -1,13 +1,20 @@
-@Grab('io.ratpack:ratpack-groovy:0.9.18')
+@GrabResolver(name='jfrog', root='http://oss.jfrog.org/oss-snapshot-local')
+@Grab('io.ratpack:ratpack-groovy:0.9.19-SNAPSHOT')
 
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
   handlers {
-    prefix("api") {
-      get {
-        render "Hello World!"
-      }
+
+    when { request.headers.'User-Agent' == "demo" }
+         {
+           get {
+             render "Hello Demo!"
+           } 
+         }
+
+    all {
+      render "Hello World!"
     }
   }
 }
