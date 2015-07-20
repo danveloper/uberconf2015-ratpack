731e82eaca96aba26472f2bf55a9c9bf59c51fa3
Dan Woods <daniel.p.woods@gmail.com>
2015-07-19 23:29:15 -0400

    COMMIT #3 - prefix handler

--- app.groovy
+++ app.groovy
@@ -4,20 +4,10 @@ import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
   handlers {
-    get("foo") {
-      render "Hello Foo!"
-    }
-    post("bar") {
-      render "Hello Bar!"
-    }
-    put("baz") {
-      render "Hello Baz!"
-    }
-    patch("qux") {
-      render "Hello Qux!"
-    }
-    delete("quux") {
-      render "Hello Quux!"
+    prefix("api") {
+      get {
+        render "Hello World!"
+      }
     }
   }
 }
